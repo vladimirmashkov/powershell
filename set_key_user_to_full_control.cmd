808abef08f901f3de8ecbdb12e49ae984ab8f900
Vladimir Mashkov <vladimir@mashkov.com>
2022-10-10 14:13:02 +0300

    Create set_key_user_to_full_control.cmd

---
+++ set_key_user_to_full_control.cmd
@@ -0,0 +1,29 @@
+::# https://superuser.com/questions/1296024/windows-ssh-permissions-for-private-key-are-too-open
+::# GUI:
+::# [File] Properties → Security → Advanced
+::#     Owner: The key's user
+::#     Permission Entries: Remove all except for the key's user
+::#     Set key's user to Full Control
+
+::# Set Key File Variable:
+    Set Key="%UserProfile%\.ssh\id_rsa"
+
+::# Remove Inheritance:
+    Icacls %Key% /c /t /Inheritance:d
+
+::# Set Ownership to Owner:
+    :: # Key's within %UserProfile%:
+         Icacls %Key% /c /t /Grant %UserName%:F
+
+    :: # Key's outside of %UserProfile%:
+         TakeOwn /F %Key%
+         Icacls %Key% /c /t /Grant:r %UserName%:F
+
+::# Remove All Users, except for Owner:
+    Icacls %Key% /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+
+::# Verify:
+    Icacls %Key%
+
+::# Remove Variable:
+    set "Key="
